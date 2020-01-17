572924cd0d6093076684b89a70ed0d027631df5a
Sudarshan Guttula <sidguttula@yahoo.com>
2020-01-16 19:58:10 -0800

    Changed "quarter" to "term" before going semester.

--- src/test/java/csns/model/academics/dao/SectionDaoTests.java
+++ src/test/java/csns/model/academics/dao/SectionDaoTests.java
@@ -1,21 +1,3 @@
-/*
- * This file is part of the CSNetwork Services (CSNS) project.
- * 
- * Copyright 2012, Chengyu Sun (csun@calstatela.edu).
- * 
- * CSNS is free software: you can redistribute it and/or modify it under the
- * terms of the GNU Affero General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
- * 
- * CSNS is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
- * more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with CSNS. If not, see http://www.gnu.org/licenses/agpl.html.
- */
 package csns.model.academics.dao;
 
 import java.util.List;
@@ -77,12 +59,6 @@ public class SectionDaoTests extends
         assert sectionDao.getSectionsByStudent( jdoe1, f10 ).size() == 1;
     }
 
-    /**
-     * To test lazy loading of collections like assignments, the test class must
-     * extend AbstractTransactionalTestNGSpringContextTests instead of
-     * AbstractTestNGSpringContextTests so Spring will run each test method in a
-     * transaction and keep the entity manager open until the method finishes.
-     */
     @Test(dependsOnMethods = "getSection")
     public void loadAssignments()
     {
