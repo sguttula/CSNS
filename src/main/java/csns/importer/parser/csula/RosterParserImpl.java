45599722a505273dbe6da72b707edec938ab6c3a
Sudarshan Guttula <sidguttula@yahoo.com>
2020-01-17 18:05:25 -0800

    Modified roster importer to handle the latest GET roster format change.

--- src/main/java/csns/importer/parser/csula/RosterParserImpl.java
+++ src/main/java/csns/importer/parser/csula/RosterParserImpl.java
@@ -1,21 +1,3 @@
-/*
- * This file is part of the CSNetwork Services (CSNS) project.
- * 
- * Copyright 2012-2016, Chengyu Sun (csun@calstatela.edu).
- * 
- * CSNS is free software: you can redistribute it and/or modify it under the
- * terms of the GNU Affero General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
- * 
- * CSNS is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
- * more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with CSNS. If not, see http://www.gnu.org/licenses/agpl.html.
- */
 package csns.importer.parser.csula;
 
 import java.util.ArrayList;
@@ -174,12 +156,6 @@ public class RosterParserImpl implements RosterParser {
         return students;
     }
 
-    /**
-     * This parser handles the format under Self Service -> Faculty Center -> My
-     * Schedule on GET. A sample record is as follows:
-     * "1 Photo 123456789 Doe,John M 4.00 Engr, Comp Sci, & Tech G2 01/05/2015".
-     * Again, not all fields may be present.
-     */
     private List<ImportedUser> parse2( String text )
     {
         List<ImportedUser> students = new ArrayList<ImportedUser>();
