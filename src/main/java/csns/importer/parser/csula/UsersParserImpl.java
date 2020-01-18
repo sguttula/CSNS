c8dc3969de11623a19565547cb5f92f10e1f5858
Sudarshan Guttula <sidguttula@yahoo.com>
2020-01-17 18:06:45 -0800

    Merged Maddie's Group code with some changes.

--- src/main/java/csns/importer/parser/csula/UsersParserImpl.java
+++ src/main/java/csns/importer/parser/csula/UsersParserImpl.java
@@ -1,21 +1,3 @@
-/*
- * This file is part of the CSNetwork Services (CSNS) project.
- * 
- * Copyright 2016, Chengyu Sun (csun@calstatela.edu).
- * 
- * CSNS is free software: you can redistribute it and/or modify it under the
- * terms of the GNU Affero General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
- * 
- * CSNS is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
- * more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with CSNS. If not, see http://www.gnu.org/licenses/agpl.html.
- */
 package csns.importer.parser.csula;
 
 import java.util.ArrayList;
@@ -27,11 +9,6 @@ import org.springframework.stereotype.Component;
 import csns.importer.ImportedUser;
 import csns.importer.parser.UserListParser;
 
-/**
- * This parser handles data copy&pasted from an Excel file produced from some
- * data source. The format is expected to be "cin first_name last_name ...".
- * Currently we only process the first three fields.
- */
 @Component("usersParser")
 public class UsersParserImpl implements UserListParser {
 
