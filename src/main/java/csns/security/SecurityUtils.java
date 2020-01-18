844d670978e7a4b704adaa0136753f688837d141
Sudarshan Guttula <sidguttula@yahoo.com>
2020-01-17 18:14:40 -0800

    Merged Maddie's Group code with some changes.

--- src/main/java/csns/security/SecurityUtils.java
+++ src/main/java/csns/security/SecurityUtils.java
@@ -1,21 +1,3 @@
-/*
- * This file is part of the CSNetwork Services (CSNS) project.
- * 
- * Copyright 2012-2016, Chengyu Sun (csun@calstatela.edu).
- * 
- * CSNS is free software: you can redistribute it and/or modify it under the
- * terms of the GNU Affero General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
- * 
- * CSNS is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
- * more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with CSNS. If not, see http://www.gnu.org/licenses/agpl.html.
- */
 package csns.security;
 
 import org.springframework.security.authentication.AuthenticationTrustResolver;
@@ -31,13 +13,6 @@ public class SecurityUtils {
 
     private static Md5PasswordEncoder passwordEncoder = new Md5PasswordEncoder();
 
-    /**
-     * <security:http> adds an AnonymousAuthenticationFilter which creates an
-     * Authentication token for anonymous users. The problem is that
-     * Authentication.isAuthenticated() will then return true even for anonymous
-     * users, so we have to use a AuthenticationTrustResolver to check for
-     * anonymous/authenticated.
-     */
     public static boolean isAnonymous()
     {
         return authenticationTrustResolver.isAnonymous(
