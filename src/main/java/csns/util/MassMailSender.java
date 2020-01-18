3c47c8a9028487f4b6f4ba7eb0995e740a0f3826
Sudarshan Guttula <sidguttula@yahoo.com>
2020-01-17 18:26:41 -0800

    Properly log array/list of email addresses.

--- src/main/java/csns/util/MassMailSender.java
+++ src/main/java/csns/util/MassMailSender.java
@@ -1,21 +1,3 @@
-/*
- * This file is part of the CSNetwork Services (CSNS) project.
- * 
- * Copyright 2012, Chengyu Sun (csun@calstatela.edu).
- * 
- * CSNS is free software: you can redistribute it and/or modify it under the
- * terms of the GNU Affero General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
- * 
- * CSNS is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
- * more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with CSNS. If not, see http://www.gnu.org/licenses/agpl.html.
- */
 package csns.util;
 
 import java.util.ArrayList;
@@ -33,12 +15,6 @@ import org.springframework.util.StringUtils;
 import csns.model.core.AbstractMessage;
 import csns.model.core.Subscribable;
 
-/**
- * Sometimes an email server may reject an email as spam if the list of
- * recipients is too long. MassMailSender breaks the list of recipients into
- * groups (of 30 by default) and send a separate email to each group. It also
- * uses bcc so a recipient won't see other people's email addresses.
- */
 @Component
 public class MassMailSender {
 
