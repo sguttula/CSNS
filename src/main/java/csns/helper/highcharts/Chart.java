56d6a4bf5c8fbd9a36d9d81fcf85d0f9153fd79e
Sudarshan Guttula <sidguttula@yahoo.com>
2020-01-17 18:01:08 -0800

    Rubric Result Graph changes.

--- src/main/java/csns/helper/highcharts/Chart.java
+++ src/main/java/csns/helper/highcharts/Chart.java
@@ -1,21 +1,3 @@
-/*
- * This file is part of the CSNetwork Services (CSNS) project.
- * 
- * Copyright 2013,2017 Chengyu Sun (csun@calstatela.edu).
- * 
- * CSNS is free software: you can redistribute it and/or modify it under the
- * terms of the GNU Affero General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
- * 
- * CSNS is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
- * more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with CSNS. If not, see http://www.gnu.org/licenses/agpl.html.
- */
 package csns.helper.highcharts;
 
 import java.util.ArrayList;
@@ -28,10 +10,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-/**
- * A Java class that can be easily serialized by Jackson into a Highcharts
- * configuration object (http://api.highcharts.com/highcharts).
- */
 public class Chart {
 
     @JsonProperty("chart")
