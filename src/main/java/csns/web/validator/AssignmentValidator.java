23deb8b10316c884e901e1614d4bf3aa6d40031e
Sudarshan Guttula <sidguttula@yahoo.com>
2020-12-11 15:55:01 -0800

    Basic assessment program management completed.

--- src/main/java/csns/web/validator/AssignmentValidator.java
+++ src/main/java/csns/web/validator/AssignmentValidator.java
@@ -1,21 +1,3 @@
-/*
- * This file is part of the CSNetwork Services (CSNS) project.
- * 
- * Copyright 2012-2016, Chengyu Sun (csun@calstatela.edu).
- * 
- * CSNS is free software: you can redistribute it and/or modify it under the
- * terms of the GNU Affero General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
- * 
- * CSNS is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
- * more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with CSNS. If not, see http://www.gnu.org/licenses/agpl.html.
- */
 package csns.web.validator;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,18 +32,6 @@ public class AssignmentValidator implements Validator {
         Assignment assignment = (Assignment) target;
         if( !StringUtils.hasText( assignment.getAlias() ) )
             assignment.setAlias( assignment.getName() );
-
-        /*
-         * Skip resource validation to allow instructors to add their own
-         * scripts and/or styling to the assignment description.
-         */
-        // if( !assignment.isOnline() )
-        // {
-        // errors.pushNestedPath( "description" );
-        // ValidationUtils.invokeValidator( resourceValidator,
-        // assignment.getDescription(), errors );
-        // errors.popNestedPath();
-        // }
     }
 
     public void validate( Assignment assignment, MultipartFile uploadedFile,
